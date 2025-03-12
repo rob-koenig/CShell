53efc883ed97d9975cfc7c569baeaf80e680658a
Rob <944fund@gmail.com>
2025-03-12 16:23:19 -0400

    revFinal

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include <signal.h>
 #include <stdio.h>
 
+void sig_handler(int signal){
+  
+} 
+
 int main( int argc, char **argv, char **envp )
 {
   struct sigaction sa;
@@ -15,5 +19,3 @@ int main( int argc, char **argv, char **envp )
 
   return sh(argc, argv, envp);
 }
-
-void sig_handler(int signal); 
